5c8fb2f62e9716f3cfc66c58f25d8b765534ab28
Chien Wei Ju <vita041833@gmail.com>
2019-01-06 14:22:24 +0800

    Update python_csv_to_array.py

--- python_csv_to_array.py
+++ python_csv_to_array.py
@@ -4,74 +4,77 @@ import pulp
 
 def csv_to_array(stock):
 
-	global data_len, d, name
+    global data_len, d, name
 
-	fh1 = open(stock, 'r', newline = '')
-	csv1 = csv.DictReader(fh1)
-	name = csv1.fieldnames
+    fh1 = open(stock, 'r', newline = '')
+    csv1 = csv.DictReader(fh1)
+    name = csv1.fieldnames
 
-	for aline in csv1: #converting csv to dictionary of arrays
+    for aline in csv1: #converting csv to dictionary of arrays
 
-		data_len += 1
+        data_len += 1
  
-		for i in range(1, len(name)):
+        for i in range(1, len(name)):
 
-			try:
-				d[name[i]] = np.append(d[name[i]], [float(aline[name[i]]) ])
+            try:
+                d[name[i]] = np.append(d[name[i]], [float(aline[name[i]]) ])
 
-			except KeyError:
-				d[name[i]] = np.array([])
+            except KeyError:
+                d[name[i]] = np.array([])
 
-	fh1.close()
+    fh1.close()
 
-def array_to_CovMatrix_Markowitz():
+class Model():
+    def __init__(self, i):
+        self.Model = i
 
-	global Matrix
+    def Markowitz(self):
 
-	Matrix = [d[name[1]]] 
+        global Matrix
 
-	for i in range(2, len(name)):
-		Matrix = np.vstack((Matrix, d[name[i]]))
+        Matrix = [d[name[1]]] 
 
-	print(np.cov(Matrix))
+        for i in range(2, len(name)):
+            Matrix = np.vstack((Matrix, d[name[i]]))
 
-def array_to_CovMatrix_Index():
+        return (np.cov(Matrix))
 
-    beta_list = []    #beta for every stock
-    e = []            #residual for every stock
+    def Index(self):
 
-    Rm = d[name[-1]]  
+        beta_list = []    #beta for every stock
+        e = []            #residual for every stock
 
-    A = np.vstack([Rm, np.ones(len(Rm))]).T 
+        Rm = d[name[-1]]  
 
-    for i in range(1,len(name)-1):
-        Rt = d[name[i]]
-        beta, alpha = np.linalg.lstsq(A, Rt, rcond = -1)[0]  
-        beta_list.append(beta)
+        A = np.vstack([Rm, np.ones(len(Rm))]).T 
 
-        residual = Rt - (alpha + beta * Rm)
-        e.append(residual)
+        for i in range(1,len(name)-1):
+            Rt = d[name[i]]
+            beta, alpha = np.linalg.lstsq(A, Rt, rcond = -1)[0]  
+            beta_list.append(beta)
 
-    beta_list.append(1.0)    
-    e = np.asarray(e)   
-    e_Var = np.var(e)     #residual variance
+            residual = Rt - (alpha + beta * Rm)
+            e.append(residual)
 
-    Rm = np.asarray(Rm) 
-    Rm_Var = np.var(Rm)   #market return variance
+        beta_list.append(1.0)    
+        e = np.asarray(e)   
+        e_Var = np.var(e)     #residual variance
 
-    global Matrix_Index
+        Rm = np.asarray(Rm) 
+        Rm_Var = np.var(Rm)   #market return variance
+
+        global Matrix_Index
     
-    Matrix_Index = [[] for x in range(len(name) - 1)]
+        Matrix_Index = [[] for x in range(len(name) - 1)]
 
-    for i in range(len(name)-1):
-        for j in range(len(name)-1):
-            if i != j:
-                Matrix_Index[i].append(beta_list[i] * beta_list[j] * Rm_Var)
-            else:
-                Matrix_Index[i].append(beta_list[i]**2 * Rm_Var + e_Var)    
-
-    print(Matrix_Index)  	
-
+        for i in range(len(name)-1):
+            for j in range(len(name)-1):
+                if i != j:
+                    Matrix_Index[i].append(beta_list[i] * beta_list[j] * Rm_Var)
+                else:
+                    Matrix_Index[i].append(beta_list[i]**2 * Rm_Var + e_Var)   
+           
+        return Matrix_Index     
 
 def objective(weight_list):
 
@@ -130,10 +133,9 @@ mb64 = ""
 d = dict()
 data_len = -1
 
-csv_to_array(stock)
-
-array_to_CovMatrix_Markowitz()
-array_to_CovMatrix_Index()
+portfolio = Model(csv_to_array(stock))
+print(portfolio.Markowitz())
+print(portfolio.Index())
 
 print(data_len)
 
